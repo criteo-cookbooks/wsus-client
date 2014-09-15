6bebd03bef159b537aed9fe6a66eac2078b61149
Baptiste Courtois <b.courtois@criteo.com>
2014-09-15 18:12:40 +0200

    Add 'windows' platform support in metadata file

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'b.courtois@criteo.com'
 license          'Apache 2.0'
 description      'Configures wsus client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'powershell'
+version          '0.1.1'
+supports         'windows'
+depends          'powershell'
