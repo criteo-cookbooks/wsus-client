c73160f167f807fb191be5b0639d80b04ce8252b
Baptiste Courtois <b.courtois@criteo.com>
2017-10-08 18:26:10 +0200

    Migrate wsus_client_update LWRP to custom resource

--- resources/update.rb
+++ resources/update.rb
@@ -18,5 +18,74 @@
 # limitations under the License.
 #
 
-actions :download, :install
-default_action [:download, :install]
+default_action %i[download install]
+
+action :download do
+  updates_to_download = updates.reject(&:IsDownloaded)
+  if updates_to_download.count != 0
+    Chef::Log.info "Windows Auto Update: #{updates_to_download.count} update(s) to download."
+    converge_by "downloading #{updates_to_download.count} update(s)" do
+      # Transforms to a new update collection
+      update_collection = ::WIN32OLE.new('Microsoft.Update.UpdateColl')
+      updates_to_download.each { |update| update_collection.Add update }
+      # Performs download
+      downloader = session.CreateUpdateDownloader
+      downloader.Updates = update_collection
+      # Verifies operation result
+      assert_result 'Download', downloader.Download
+    end
+  end
+end
+
+action :install do
+  downloaded_updates = updates.select(&:IsDownloaded)
+  if downloaded_updates.count != 0
+    Chef::Log.info "Windows Auto Update: #{downloaded_updates.count} update(s) to install."
+    converge_by "installing #{downloaded_updates.count} update(s)" do
+      # Transforms to a new update collection
+      update_collection = ::WIN32OLE.new('Microsoft.Update.UpdateColl')
+      downloaded_updates.each do |update|
+        unless update.EulaAccepted
+          converge_by "accepting EULA for #{update.Title}" do
+            update.AcceptEula
+          end
+        end
+        update_collection.Add update
+      end
+      # Performs install
+      installer = session.CreateUpdateInstaller
+      installer.ForceQuiet = true
+      installer.Updates = update_collection
+      # Verifies operation result
+      assert_result 'Installation', installer.Install
+    end
+  end
+end
+
+action_class do
+  def whyrun_supported?
+    true
+  end
+
+  # ResultCode values: http://msdn.microsoft.com/aa387095
+  SUCCESS_CODE = 2
+  def assert_result(action, result)
+    return unless result.HResult != 0 || result.ResultCode != SUCCESS_CODE
+    raise "#{action} failed. (Error code #{result.HResult})"
+  end
+
+  def updates
+    node.run_state['wsus_client_updates'] ||= [].tap do |updates|
+      # Searches non installed updates
+      search_result = session.CreateUpdateSearcher.Search 'IsInstalled=0'
+      # Transforms to ruby array for future use
+      search_result.Updates.each { |update| updates << update }
+    end
+  end
+
+  def session
+    require 'win32ole'
+    # API documentation: http://msdn.microsoft.com/aa387099
+    @session ||= ::WIN32OLE.new('Microsoft.Update.Session')
+  end
+end
